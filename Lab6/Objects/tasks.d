f478e639b83a18f21d9d49b45fc7367110ff106f
OmarElbanna <elbanna16@gmail.com>
2023-03-30 01:09:49 +0200

    Uploaded Lab6

---
+++ Lab6/Objects/tasks.d
@@ -0,0 +1,26 @@
+./objects/tasks.o: \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\Source\tasks.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\Source\include\FreeRTOS.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  RTE\RTOS\FreeRTOSConfig.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\CMSIS\RTOS2\FreeRTOS\Include\freertos_evr.h \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\Source\include\projdefs.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\Source\include\portable.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\Source\include\deprecated_definitions.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\Source\portable\GCC\ARM_CM4F\portmacro.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\Source\include\mpu_wrappers.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\Source\include\task.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\Source\include\list.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\Source\include\timers.h \
+  C:\Users\Omar\ Elbanna\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.0.1\Source\include\stack_macros.h
